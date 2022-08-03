d1264e447398d05614cb6941b6713f2c554a587c
a3465134651 <a3465134651@gmail.com>
2022-08-03 14:44:34 +0800

    add progressbar

--- read.py
+++ read.py
@@ -1,13 +1,16 @@
 import time
+import progressbar
 
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value = 1000000)
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 #count = count + 1
-		if count % 1000 == 0:
-		    print(len(data))
+		# if count % 1000 == 0:
+		#     print(len(data))
+		bar.update(count)
 print('檔案讀取完了，總共有', len(data), '筆資料')
 
 sum_len = 0
